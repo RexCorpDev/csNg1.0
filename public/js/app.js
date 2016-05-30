c1e6b059ff7f1b8d237a0502a36e3fcb8b73ab6e
RexCorpDev <rexcorpfx@gmail.com>
2016-05-30 13:27:11 -0700

    added notes in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -24,3 +24,25 @@
     }
   ];
 })();
+
+// NOTES:
+// ng-controller='MainController as main'
+//
+// <em class='pull left'></em> this allows for pull-left & pull-right WITHOUT distoring other elements.
+//
+// ul class='list-group'
+// ---------------------------------------------------
+// FILTERS:
+//  >date = {{'1231512312' | date:'MM/dd/yyyy'}}
+//
+//  >upper & lower case = {{'octagon gem' | uppercase}}
+//
+//  >limitTo = {{'My description' | limitTo:8}}  *this only displays the first 8 characters of the string.
+//  {{li ng-repeat='product in store.products | limitTo:3'}} * only displays the first 3 repeats
+//
+//  >orderby = {{li ng-repeat='products in store.products | orderBy:-price'}} *orders products by their respective price
+//
+// *FILTER summary = limitTo, orderby, date:'MM/dd/yyyy', uppercase, lowercase
+//
+// ---------------------------------------------------
+//
